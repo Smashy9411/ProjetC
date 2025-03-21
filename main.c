ad2015e9b87f45b4cb25bb7ce95592e21fb17e06
Mathis <mathis.poisson@efrei.net>
2025-03-21 08:41:06 +0100

    debut du main

--- main.c
+++ main.c
@@ -1,3 +1,12 @@
-//
-// Created by nicol on 21/03/2025.
-//
+#include <stdio.h>
+
+int main(void) {
+    int choice;
+    printf("Veuillez choisir une option :\n 1.  Ouvrir une image \n 2.  Sauvegarder une image\n 3.  Appliquer un filtre\n 4.  Afficher les informations de l'image \n 5.  Quitter\n ");
+    scanf("%d",&choice);
+    if (choice == 1) {
+
+    }
+
+    return 0;
+}
